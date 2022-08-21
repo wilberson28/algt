c41c645a7b8514bb0c75211909925ed273f88440
wilberson28 <110622550+wilberson28@users.noreply.github.com>
2022-08-21 18:35:39 -0500

    Create lib.cpp

---
+++ lib.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "lib.hpp"
+#include <vector>
+int lineal_search(const std::vector<int>&v, int x){
+	int val=x;
+	for (int i=0; i<v.size(); i++){
+		if(v[i]==x){
+			return i;
+			
+		}
+		
+	
+		
+	
+	}
+	
+	
+	
+}
